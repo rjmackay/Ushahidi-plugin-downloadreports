40e7897044934edccda1eb96a993fac8c5058b78
Robbie Mackay <rm@robbiemackay.com>
2012-10-10 15:14:46 +1300

    Wrap user data in CDATA blocks for KML

--- views/kml.php
+++ views/kml.php
@@ -14,13 +14,6 @@
 				<?php echo "ff" . $category->category_color;?>
 				</color>
 				<scale>0.8</scale>
-				<Icon>
-					<href>
-					<?php
-						echo url::base() . "plugins/kml/views/circle_border.png";
-					?>
-					</href>
-				</Icon>
 			</IconStyle>
 		</Style>
 		<?php
@@ -38,14 +31,12 @@
 				break;
 			}
 			?>
-			<name>
-				<?php echo htmlspecialchars($item->incident_title);?>
-			</name>
+			<name><![CDATA[<?php echo $item->incident_title; ?>]]></name>
 			<description>
-				<?php
-					echo htmlspecialchars(text::limit_words($item->incident_description, 50, "..."));
-					echo "<BR /><a href=\"" . url::base() . 'reports/view/' . $item->id . "\">More...</a>";
-				?>
+				<![CDATA[<?php
+					echo text::limit_words($item->incident_description, 50, "...");
+					echo "<br /><a href=\"" . url::base() . 'reports/view/' . $item->id . "\">More...</a>";
+				?>]]>
 			</description>
 			<Point>
 				<coordinates>
